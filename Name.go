1b009a03b257285d70b1085d39c73be153dd64f8
mcsid5397 <mpraveenkumar5397@gmail.com>
2023-02-14 18:37:34 +0530

    String & Variables

---
+++ Name.go
@@ -0,0 +1,12 @@
+package main
+import ("fmt")
+
+func main() {
+  var firstname string = "Himanshi" //type is string
+  var lastname = "Singh" //type is inferred
+  rollno := 22 //type is inferred
+
+  fmt.Println(firstname)
+  fmt.Println(lastname)
+  fmt.Println(rollno)
+}
