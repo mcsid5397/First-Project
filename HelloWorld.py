71acd43b62e5518dda68d3b15ec95cbed54dba83
mcsid5397 <mpraveenkumar5397@gmail.com>
2025-08-07 10:45:29 +0530

    Hari sir's class. . .

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,1 @@
-print(f"Goof to see y'all visiting my first-project page, please put a word, I'll be more than happy to communicate back!...")
+print(f"Good to see y'all visiting my first-project page, please put a word, I'll be more than happy to communicate back!...")
